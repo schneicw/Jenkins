6e953ed13caa6a02e981cf83cb8fe484ef019989
Kurt Dawiec <kurtmdawiec@gmail.com>
2020-08-06 17:04:23 -0400

    fixed tests AGOIBWVJHSBCKNBSKJDBJSN

--- src/test/java/dev/team/daotests/DaoTests.java
+++ src/test/java/dev/team/daotests/DaoTests.java
@@ -1,39 +1,31 @@
 package dev.team.daotests;
 
-
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
 
 import dev.team.daos.RoomDAO;
 import dev.team.daos.RoomDAOImpl;
 import dev.team.enitities.Room;
 
-@TestMethodOrder(OrderAnnotation.class)
-public class DaoTest {
+@TestMethodOrder(OrderAnnotation.class) 
+class DaoTests {
 
 	public static RoomDAO rdao = RoomDAOImpl.getRoomDAO();
 	
-	@BeforeAll
-	static void setUp() {
-		Map<Integer, Room> room_table = new HashMap<Integer, Room>();
-		
-		int idMaker = 0;
-	}
 	
 	@Test
 	@Order(1)
 	void createRoom() {
-		Room room1 = new Room(0, 800, 600);
+		Room room1 = new Room(1, 800, 600);
 		Room room2 = new Room(0,1000, 1000);
 		
 		rdao.createRoom(room1);
@@ -68,5 +60,4 @@ public class DaoTest {
 		
 		Assertions.assertEquals(900, room.getRent());
 	}
-
 }
