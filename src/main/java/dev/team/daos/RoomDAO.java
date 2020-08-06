b21fa6a6386123ad715f8a49831f96c0f8456f93
NoahWhite1 <41762591+NoahWhite1@users.noreply.github.com>
2020-08-06 15:59:11 -0400

    created room entity and created dao functionality

--- src/main/java/dev/team/daos/RoomDAO.java
+++ src/main/java/dev/team/daos/RoomDAO.java
@@ -1,5 +1,12 @@
 package dev.team.daos;
 
-public interface RoomDAO {
+import java.util.Set;
 
+import dev.team.enitities.Room;
+
+public interface RoomDAO {
+	Room createRoom(Room room);
+	Set<Room> getAllRooms();
+	Room getRoomByrId(int id);
+	Room updateRoom(Room room);
 }
